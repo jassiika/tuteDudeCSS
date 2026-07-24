776e5eb627456346a87e16c848fd98a1a7a05d2b
Jasika <jasikanarang2006@gmail.com>
2026-07-24 06:46:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,72 +1,31 @@
-const n1= 73;
-
-// Calculating the first n numbers sum
-var sum=0;
-for(let i=1;i<=n1;i++){
-    sum+=i;
-}
-console.log(`The sum of first ${n1} numbers is ${sum}`);
-
-// Printing the table of n
-const n2=153
-for(let i=1;i<11;i++){
-    console.log(`${n2} * ${i} = ${n2*i}`);
+//finding maximum amoung the elements in arrray
+let dummy=[32,54,3,21,89,26];
+function maximum(array){
+    let max=array[0];
+    for(let ele of array){
+        if(ele>max){
+            max=ele;
+        }
+    }return max;
 }
 
-// Prime no. check
-const n3= 54;
-if(n3==0||n3==1){
-    console.log(`${n3} is not prime number.`);
-}
-let flag =0;
-for(let i=2;n3>=2 && i*i<=n3;i++){
-    if(n3%i==0){
-        console.log(`${n3} is not prime number.`);
-        flag=1;
-        break;
-    }
-}
-if(!flag)
-    console.log(`${n3} is a prime number.`);
+//finding sum of elements of array
+console.log(maximum(dummy));
+const totalsum=(array)=>{
+    let sum=0;
+    for(let i of array){
+        sum+=i;
+    }return sum;
+};
+console.log(totalsum(dummy));
 
-// finding sum of digits
-var n4=5432;
-let original =n4;
-let sum2=0;
-while(n4){
-    sum2+=n4%10;
-    n4=Math.floor(n4/10);
-}
-console.log("sum of digits of number",original,"is",sum2);
-
-//factors
-const n6=76;
-console.log(`Factors of ${n6} are:`);
-for(let i=1;i<=n6;i++){
-    if(n6%i==0){
-        console.log(i);
-    }
-}
-
-//armstrong check
-const n5=153;
-let temp=n5;
-let count=0;
-while(temp){
-    count++;
-    temp=Math.floor(temp/10);
-}
-temp=n5;
-let sum3=0;
-while(temp){
-    let digit=temp%10;
-    let product=1;
-    for(let i=1;i<=count;i++){
-        product*=digit;
-    }sum3+=product;
-    temp=Math.floor(temp/10);
-}if(sum3==n5) {
-    console.log(n5,"is a Armstrong number.");
-}else{
-    console.log(n5,"is not a Armstrong number.");
-}
+//counting odd no. in array
+const countodd=function(array){
+    let count=0;
+    for(let ele of array){
+        if(ele%2!=0){
+            count++;
+        }
+    }return count;
+};
+console.log(countodd(dummy));
