0b674b4cb5edee1e3c65ec6fd5b38ab2fc0cb7ec
Jasika <jasikanarang2006@gmail.com>
2026-07-31 17:47:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
 function division (a,b){
   return new Promise ((resolve,reject) => {
     console.log(`Dividing ${a} by ${b}...`);
-    setTimeout(()=>{
-      if(b===0){
+      if(b==0){
         reject("Division by zero is not allowed.");
       }else{
         resolve(a/b);
       }
-    },1000);
   });
 };
 
 division(10,2)
   .then((result)=>{
     console.log("Result:",result);
-    return division(10,0);
+    return division(100,2);
+  })
+  .then((result)=>{
+    console.log("Result:",result);
+    return division(10,5);
+  })
+  .then((result)=>{
+    console.log("Result:",result);
+    return division(30,6);
+  })
+  .then((result)=>{
+    console.log("Result:",result);
+    return division(40,0);
   })
   .then((result)=>{
     console.log("Result:",result);
